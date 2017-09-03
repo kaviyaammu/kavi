7e1c13a84ac3b6a86f9a9c73fec52705846324ef
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2017-09-03 13:09:21 +0530

    Create positive or negative.c

---
+++ positive or negative.c
@@ -0,0 +1,18 @@
+# kavi
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a number");
+scanf("%d",&a);
+if(a<=0)
+{
+if(a==0)
+printf("You entered 0.");
+else 
+printf("You entered -ve no.");
+}
+else
+printf("You entered +ve no.);
+return 0;
+}
