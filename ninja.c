8a055fd189a6dc109eefa46a2c9c74316d5b6694
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2018-01-26 16:43:14 +0530

    Create ninja.c

---
+++ ninja.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,k,n,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	scanf("%d%d",&t,&k);
+		printf("the difference between the soldiers :%d",k-t);
+	}
+	
+	return 0;
+}
